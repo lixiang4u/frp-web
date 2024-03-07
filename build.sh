766ce9e5c688229c7c51d539455e680308f95649
game.desktop <game.desktop@example.com>
2024-03-07 09:21:59 +0800

    修改名称

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ npm run build
 echo '开始打包可执行程序'
 cd $PROJECT_PATH
 go get
-GOOS=linux GOARCH=amd64 go build -o frp-web-linux-amd64-v0.1
+GOOS=linux   GOARCH=amd64 go build -o frp-web-linux-amd64-v0.1
 GOOS=windows GOARCH=amd64 go build -o frp-web-windows-amd64-v0.1.exe
-GOOS=darwin GOARCH=amd64 go build -o frp-web-darwin-amd64-v0.1.app
+GOOS=darwin  GOARCH=amd64 go build -o frp-web-darwin-amd64-v0.1
 
 ls -lh  frp-web*
 
