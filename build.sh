99a644c153bd87810c5dc09d6145ccd5ad8f2441
game.desktop <game.desktop@example.com>
2024-03-08 16:27:41 +0800

    修改打包脚本

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=v0.1
+
 PROJECT_PATH=$PWD
 
 echo '开始打包web'
@@ -10,9 +12,9 @@ npm run build
 echo '开始打包可执行程序'
 cd $PROJECT_PATH
 go get
-GOOS=linux   GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o frp-web-linux-amd64-v0.1
-GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o frp-web-windows-amd64-v0.1.exe
-GOOS=darwin  GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o frp-web-darwin-amd64-v0.1
+GOOS=linux   GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o frp-web-linux-amd64-$VERSION
+GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o frp-web-windows-amd64-$VERSION.exe
+GOOS=darwin  GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o frp-web-darwin-amd64-$VERSION
 
 
 cd $PROJECT_PATH
